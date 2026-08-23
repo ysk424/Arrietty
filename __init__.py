181164c937b7593a8ccb8b0baad72abfc84fa2d8
ysk424 <noreply>
2026-08-24 08:11:24 +0900

    Initialize Arrietty Blender extension

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2026 ysk424
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Arrietty Blender Extension.
+
+The project is in its initial scaffolding stage.
+"""
+
+
+def register() -> None:
+    """Register the extension."""
+
+
+def unregister() -> None:
+    """Unregister the extension."""
